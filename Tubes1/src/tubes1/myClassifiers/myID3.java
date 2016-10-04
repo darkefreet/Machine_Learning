3bf3be7977220eb126e04ebb2c0218d6f966b167
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-10-04 10:53:44 +0700

    implement entropy

--- Tubes1/src/tubes1/myClassifiers/myID3.java
+++ Tubes1/src/tubes1/myClassifiers/myID3.java
@@ -6,6 +6,7 @@
 package tubes1.myClassifiers;
 
 import weka.classifiers.Classifier;
+import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
 
@@ -25,4 +26,35 @@ public class myID3 extends Classifier {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    public double infoGain(Instances instances, Attribute attribute, double entropyOfSet){
+        return 0;
+    }
+    
+    public double entropy(Instances instances){
+        double result = 0;
+        double[] proportion = calculateProportion(instances);
+        for (double p : proportion){
+            result -= Math.log(p)/Math.log(2)*p;
+        }
+        return result;
+    }
+    
+    public double [] calculateProportion(Instances instances){
+        int numClasses = instances.classAttribute().numValues();
+        int numInstances = instances.numInstances();
+        double [] result = new double[numClasses];
+        int [] num = new int[numClasses];
+        for (int i=0;i<numClasses;i++){
+            num[i]=0;
+        }
+        for (int i=0;i<numInstances;i++){
+            Instance instance = instances.instance(i);
+            int classIndex = instance.classIndex();
+            num[classIndex]++;
+        }
+        for (int i=0;i<numClasses;i++){
+            result[i] = num[i]/numInstances;
+        }
+        return result;
+    }
 }
