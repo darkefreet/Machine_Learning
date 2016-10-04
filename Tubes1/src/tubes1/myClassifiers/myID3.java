f69d9d2114496a16ea9ec8c625aed19e1f9e2c68
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-10-04 10:02:35 +0700

    add skeleton for myClassifier: myID3 dan myC45

---
+++ Tubes1/src/tubes1/myClassifiers/myID3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes1.myClassifiers;
+
+import weka.classifiers.Classifier;
+import weka.core.Instance;
+import weka.core.Instances;
+
+/**
+ *
+ * @author nim_13512501
+ */
+public class myID3 extends Classifier {
+
+    @Override
+    public void buildClassifier(Instances i) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public double classifyInstance(Instance instance){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
