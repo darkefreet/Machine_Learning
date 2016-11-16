4d0a00f130f3b5619b26bf87b33a9f7fc04de6eb
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-11-16 17:32:29 +0700

    add initializeCentroids and assignClusters

--- Tubes2/src/tubes2/myClusterers/myKMeans.java
+++ Tubes2/src/tubes2/myClusterers/myKMeans.java
@@ -5,6 +5,9 @@
  */
 package tubes2.myClusterers;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 import weka.core.Instance;
 import weka.core.Instances;
 
@@ -25,16 +28,66 @@ public class myKMeans extends weka.clusterers.AbstractClusterer {
         this.distanceFunction = new EuclideanDistance();
     }
     
-        Instance [] clusters;
+    Instance [] centroids;
+    Instances [] clusters;
     
     @Override
     public int clusterInstance(Instance instance)
                     throws java.lang.Exception{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    /**
+     * initializes centroids randomly
+     * @param i 
+     */
+    public void initializeCentroids(Instances i) throws Exception{
+        int n = i.numInstances();
+        if (n<k)
+            throw new Exception("n<k");
+        
+        Set<Integer> centroidIndexSet = new HashSet<>();
+        Random random = new Random();
+        for (int j=0;j<k;j++){
+            int centroidIndex;
+            do{
+                centroidIndex = random.nextInt(n);
+            }while(centroidIndexSet.contains(centroidIndex));
+            
+            centroids[j]=i.instance(centroidIndex);
+        }
+    }
+    
+    public void assignClusters(Instances instances) throws Exception {
+        clusters = new Instances[k];
+        for (int i=0;i<instances.numInstances();i++){
+            assignCluster(instances.instance(i));
+        }
+    }
+    
+    public void assignCluster(Instance instance) throws Exception{
+        int clusterNum = nearestCentroid(instance);
+        
+        clusters[clusterNum].add(instance);
+    }
+    
+    public int nearestCentroid(Instance instance) throws Exception{
+        int iChosen = -1;
+        double minDistance = Double.MAX_VALUE;
+        for (int i=0;i<centroids.length;i++){
+            double distance = distanceFunction.distanceOf(instance, centroids[i]);
+            if (distance<minDistance){
+                iChosen = i;
+                minDistance = distance;
+            }
+        }
+        return iChosen;
+    }
 
     @Override
     public void buildClusterer(Instances i) throws Exception {
+        initializeCentroids(i);
+        assignClusters(i);
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
