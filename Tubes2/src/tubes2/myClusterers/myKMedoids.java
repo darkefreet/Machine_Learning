cd7fb15c9dfa34d39af150961de992c319a33c04
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-11-16 17:00:47 +0700

    init Tubes2

---
+++ Tubes2/src/tubes2/myClusterers/myKMedoids.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes2.myClusterers;
+
+import weka.core.Instance;
+import weka.core.Instances;
+
+/**
+ *
+ * @author nim_13512501
+ */
+public class myKMedoids extends weka.clusterers.AbstractClusterer{
+    
+    int k;
+    public myKMedoids(int k){
+        this.k = k;
+    }
+    
+    @Override
+    public int clusterInstance(Instance instance)
+                    throws java.lang.Exception{
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void buildClusterer(Instances i) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int numberOfClusters() throws Exception {
+        return k;
+    }
+    
+}
