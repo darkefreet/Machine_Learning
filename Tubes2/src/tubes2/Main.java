ae36c20dced97f00f473925469264be953fe8a20
Wilhelmus Tanujaya <wilhelm_andrean_22@yaho.com>
2016-11-20 11:28:18 +0700

    normalisasi value numeric

--- Tubes2/src/tubes2/Main.java
+++ Tubes2/src/tubes2/Main.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import tubes2.myClusterers.myAgnes;
@@ -32,6 +33,50 @@ public class Main {
         return inputReader;
     }
     
+    public static void normalizeData(Instances data, ArrayList<Integer> numericIndex){
+        ArrayList<Double> mean = new ArrayList();
+        ArrayList<Double> standardDef = new ArrayList();
+        
+        //INISIALISASI
+        for(int i = 0;i<numericIndex.size();i++){
+            mean.add(0.0);
+            standardDef.add(0.0);
+        }
+        //HITUNG MEAN
+        for(int i = 0; i<data.numInstances();i++){
+            for(int j = 0;j<numericIndex.size();j++){
+                mean.set(j,(mean.get(j)+data.instance(i).value(numericIndex.get(j))/data.numInstances()));
+            }
+        }
+        
+        //HITUNG STANDAR DEVIASI
+        for(int i = 0; i<data.numInstances();i++){
+            for(int j = 0;j<numericIndex.size();j++){
+                standardDef.set(j,standardDef.get(j)+((Math.abs(data.instance(i).value(numericIndex.get(j))-mean.get(j)))/data.numInstances()));
+            }
+        }
+        
+        //UBAH NILAI INSTANCE MENJADI Z-SCORE
+        for(int i = 0; i<data.numInstances();i++){
+            for(int j = 0;j<numericIndex.size();j++){
+                data.instance(i).setValue(numericIndex.get(j),(data.instance(i).value(numericIndex.get(j)) - mean.get(j))/standardDef.get(j));
+            }
+        }
+    }
+    
+    public static boolean isNumeric(String str)  
+    {  
+      try  
+      {  
+        double d = Double.parseDouble(str);  
+      }  
+      catch(NumberFormatException nfe)  
+      {  
+        return false;  
+      }  
+      return true;  
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -45,6 +90,20 @@ public class Main {
         //LOAD FILE
         BufferedReader datafile = readDataFile("data/"+filename+".arff");
         Instances data = new Instances(datafile);
+        
+        ArrayList<Integer> numericIdx = new ArrayList<Integer>();
+        for (int i = 0;i < data.numAttributes();i++){
+            if(data.attribute(i).isNumeric()){
+                numericIdx.add(i);
+            }
+        }
+        System.out.println();
+        System.out.println("\n----SEBELUM NORMALISASI-----");
+        System.out.println(data);
+        normalizeData(data,numericIdx);
+        System.out.println("\n----SETELAH NORMALISASI-----");
+        System.out.println(data);
+        
         //END OF LOAD FILE
         
         Clusterer [] clusterers = {
@@ -56,6 +115,7 @@ public class Main {
         for (Clusterer clusterer: clusterers){
             try {
                 clusterer.buildClusterer(data);
+                System.out.println("\n\n----HASIL CLUSTERING-----");
                 System.out.println(clusterer);
             } catch (Exception ex) {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
