8e10f07d71772a996a2bc39ba75a1ddacd3a607c
Wilhelmus Tanujaya <wilhelm_andrean_22@yaho.com>
2016-11-24 20:31:49 +0700

    distance gabungan gak berguna

---
+++ Tubes2/src/tubes2/myClusterers/DistanceCombination.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes2.myClusterers;
+
+import weka.core.Instance;
+
+/**
+ *
+ * @author Hp
+ */
+public class DistanceCombination implements DistanceFunction{
+    @Override
+    public double distanceOf(Instance a, Instance b){
+        double ret = 0.0;
+        return ret;
+    }
+}
